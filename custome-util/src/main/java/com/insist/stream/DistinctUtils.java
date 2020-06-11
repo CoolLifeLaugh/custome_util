ee9be66ec5bfffa777a3e71d525f2ecdce120930
fanping.jwq <fanping.jwq@alibaba-inc.com>
2020-06-11 22:57:18 +0800

    custome util

---
+++ custome-util/src/main/java/com/insist/stream/DistinctUtils.java
@@ -0,0 +1,18 @@
+package com.insist.stream;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * @author fanping.jwq@alibaba-inc.com
+ * @Date 2020-06-11 22:56
+ */
+public class DistinctUtils {
+
+    public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
+        Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+        return object -> seen.putIfAbsent(keyExtractor.apply(object), Boolean.TRUE) == null;
+    }
+}
